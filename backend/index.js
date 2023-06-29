478d9e9ba787b6e6d38e6e5ba615c4a371dc9064
Paulo <paulocorreapc93@gmail.com>
2023-06-29 14:15:22 -0400

    teste de conexão com banco de dados concluido com sucesso

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,14 @@ app.get("/", (req, res) => {
     res.json("funcionou")
 })
 
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
 app.listen(5000, () => {
   console.log("conectado ao banco de dados");
 });
