950f4c22053cc24b2503835832374e76b55282a2
Paulo <paulocorreapc93@gmail.com>
2023-06-29 13:33:21 -0400

    teste de requisição para a porta 5000 concluida com sucesso

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("conectado ao banco de dados")
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "senhamysql",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+    res.json("funcionou")
 })
+
+app.listen(5000, () => {
+  console.log("conectado ao banco de dados");
+});
