413c6a818285559882bafbf26e3d02c3e3c49a7a
Paulo <paulocorreapc93@gmail.com>
2023-07-03 11:12:37 -0400

    adicionada função para deletar livro

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,24 @@ app.get("/books", (req, res) => {
   });
 });
 
+app.delete("books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if(err) return res.json(err);
+    return res.json("Book has been delete successfully")
+  })
+});
+
 app.post("/books", (req, res) => {
   const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.price, req.body.cover];
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
