e47ab80d2a17279214e4ac88aa4ce968cb185e0d
Paulo <paulocorreapc93@gmail.com>
2023-07-02 23:36:32 -0400

    adicionado botão para adicionar livro, com evento 'handleClick'

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -19,6 +19,8 @@ const Add = () => {
       <input type="text" placeholder="desc" onChange={handleChange} name="desc" />
       <input type="number" placeholder="price" onChange={handleChange} name="price" />
       <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
+
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
