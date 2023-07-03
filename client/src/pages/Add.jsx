4bd512dd0111524800554d095ac52fda4e5ee483
Paulo <paulocorreapc93@gmail.com>
2023-07-02 23:30:07 -0400

    criado estado para armazenar variavel e função para armazenar dados dos livros

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Add = () => {
-  return (
-    <div className='form'>
-      <h1>Add New Book</h1>
-      <input type="text" placeholder='title' />
-      <input type="text" placeholder='desc' />
-      <input type="number" placeholder='price' />
-      <input type="text" placeholder='cover' />
-    </div>
-  )
-}
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
 
-export default Add
+  return (
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="desc" />
+      <input type="number" placeholder="price" />
+      <input type="text" placeholder="cover" />
+    </div>
+  );
+};
+
+export default Add;
