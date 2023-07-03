8ce8d6ebb6f7a0e2252aef5be0f58206c39855e7
Paulo <paulocorreapc93@gmail.com>
2023-07-02 23:27:26 -0400

    adicionadas as tags para o input dos livros

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Add = () => {
   return (
     <div className='form'>
-      
+      <h1>Add New Book</h1>
+      <input type="text" placeholder='title' />
+      <input type="text" placeholder='desc' />
+      <input type="number" placeholder='price' />
+      <input type="text" placeholder='cover' />
     </div>
   )
 }
