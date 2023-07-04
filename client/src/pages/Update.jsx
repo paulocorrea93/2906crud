a9b080e0b8462fd5ab853a5fb524a42548040655
Paulo <paulocorreapc93@gmail.com>
2023-07-04 10:03:13 -0400

    importado 'useLocation'

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Update = () => {
   const [book, setBook] = useState({
