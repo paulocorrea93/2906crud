4ab282c632a3ac8a0bbab33bb701f256e129251f
Paulo <paulocorreapc93@gmail.com>
2023-07-04 09:42:19 -0400

    estruturado o componente 'update'

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Update = () => {
-  return (
-    <div>
-      Update
-    </div>
-  )
-}
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
 
-export default Update
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Update The Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
+};
+
+export default Update;
